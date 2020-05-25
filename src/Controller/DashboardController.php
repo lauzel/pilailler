bb6e711a6cbc7ac09b1610cabec4e01aeac372a6
Alexandre P <alexandreponcet.mail@gmail.com>
2020-05-25 19:03:09 +0200

    add refresh on dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\TankStats;
 use App\Repository\MetricsRepository;
 use App\Repository\TankStatsRepository;
 use App\Service\DHT11Manager;
+use App\Service\TankFillingManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -32,5 +35,28 @@ class DashboardController extends AbstractController
             'metrics' => $metrics,
             'tankFilling' => $tankFilling->getFillingPercent()
         ]);
+    }
+
+    /**
+     * @Route("/dashboard/refreshtankstat", name="refreshtankdashboard")
+     */
+    public function refreshTankStat(TankFillingManager $tankFillingManager, EntityManagerInterface $manager)
+    {
+        try {
+
+            $percent = $tankFillingManager->getFillingPercent();
+
+            $data = new TankStats();
+            $data->setCreatedAt(new \DateTime('now'));
+            $data->setFillingPercent($percent);
+
+            $manager->commit($data);
+            $manager->flush();
+        } catch (\Exception $e) {
+
+        }
+
+        return $this->redirectToRoute('dashboard');
+
     }
 }
